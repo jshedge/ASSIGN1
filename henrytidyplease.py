cf3fe012333fad4b504a6237eb3e2af9222951c2
henrygsys <89590175+henrygsys@users.noreply.github.com>
2022-03-14 08:40:52 +0000

    Update henrytidyplease.py

--- henrytidyplease.py
+++ henrytidyplease.py
@@ -6,46 +6,37 @@ Created on Sat Mar 12 15:50:31 2022
 @author: Jamie
 """
 
-question = ("Type in any word")
-print (question)
-answer = input()
-print ("Your word contains", len(answer), ("letters"))
+from tenacity import retry
 
-print ("type a number")
-response = input()
-number = int(response)
-print ("The total number of letters in your word added to your number is...", number + (len(answer)))
 
-#################################################################
-#You can use  %s in place of "," or "+". See below
+question_list = ['What is your favourite food?', 'What is your favourite film?']
 
-question = ("Type in any word")
-print (question)
-answer = input()
-print ("Your word contains", len(answer), ("letters"))
+def question_response(question):
+    print (f"Please type the answer to the following question: {question}")
+    answer = input()
+    print ("Your word contains", len(answer), ("letters"))
+    return answer
+    
+def number_response(answer):
+    print ("type a number")
+    response = input()
+    number = int(response)
+    finalanswer = (number + (len(answer)))
+    first_answer = f"The total number of letters in your word added to your number is {finalanswer}"
+    print (first_answer)
+    return number
 
-print ("type a number")
-response = input()
-number = int(response)
-finalanswer = (number + (len(answer)))
-print ("The total number of letters in your word added to your number is... %s" % finalanswer)
+def second_number_response(number, answer):
+    print ("type another number")
+    response2 = input()
+    number2 = int(response2)
+    finalanswer = (number + (len(answer)))
+    finalanswer2 = (number2 + (len(answer)))
+    second_answer =  f"The total number of letters in your word added to your number and your second number is {finalanswer} and {finalanswer2}"
+    return print(second_answer)
 
-#################################################################
 
-question = ("Type in any word")
-print (question)
-answer = input()
-print ("Your word contains", len(answer), ("letters"))
-
-print ("type a number")
-response = input()
-number = int(response)
-print ("type another number")
-response2 = input()
-number2 = int(response2)
-finalanswer = (number + (len(answer)))
-finalanswer2 = (number2 + (len(answer)))
-print ("The total number of letters in your word added to your number and your second number is... %s and %s" % (finalanswer, finalanswer2))
-                                                                                         
-                                                                                
-                                                                                    
+for question in question_list:
+    answer = question_response(question)
+    number = number_response(answer)
+    second_number_response(number, answer)
